86fc4f94e65b2d624c907c2a619f181666ae18f6
FINNAPPELO <99080583@mydavinci.nl>
2024-09-12 11:03:33 +0200

    Min en Max

--- module 1/deel 2/input en if-statement.py
+++ module 1/deel 2/input en if-statement.py
@@ -4,9 +4,17 @@ if a>b:
     max =a
     min = b
     print(f"a is het grootste getaal {max}")
+    print(f"Het minimum is:{min}")
+    print(f"Het maximum is:{max}")
 elif a<b:
     min=a
     max = b 
     print(f"a is het klijnste getal {min}")
+    print(f"Het minimum is:{min}")
+    print(f"Het maximum is:{max}")
 else:
+    min = a
+    max = b
     print("a en b zijn de zelfte waarde ")
+    print(f"Het minimum is:{min}")
+    print(f"Het maximum is:{max}")
