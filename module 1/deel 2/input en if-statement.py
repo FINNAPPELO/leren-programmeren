8af2d87c73b92ab6502c86e6c2c6380a03ae4ae3
FINNAPPELO <99080583@mydavinci.nl>
2024-09-12 10:56:36 +0200

    elif-statement

--- module 1/deel 2/input en if-statement.py
+++ module 1/deel 2/input en if-statement.py
@@ -2,4 +2,9 @@ a = int(input("vul getal 1 in : "))
 b = int(input("vul getal 2 in : "))
 if a>b:
     max =a
+    min = b
     print(f"a is het grootste getaal {max}")
+elif a<b:
+    min=a
+    max = b 
+    print(f"a is het klijnste getal {min}")
