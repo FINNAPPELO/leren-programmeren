aaf3bce795947c2d3600e381cb8e20ae8a80f92e
FINNAPPELO <99080583@mydavinci.nl>
2024-09-13 10:26:24 +0200

    Create 1 order_bug.py

---
+++ module 1/deel 3/1 order_bug.py
@@ -0,0 +1,9 @@
+getal_1= int(input("voer hier uw eerste getal in? "))
+getal_2 = int(input("voer hier uw tweede tetal in? "))
+
+if getal_2 <= 0:
+    print("Kan niet delen door 0")
+else:
+     resultaat =(getal_1/getal_2)
+     print(f"{getal_1} gedeeld door {getal_2} is gelijk aan {resultaat}")
+   
