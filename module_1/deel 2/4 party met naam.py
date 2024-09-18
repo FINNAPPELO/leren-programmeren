d790af91eb6d8d130b6fbae9108260311449aa31
FINNAPPELO <99080583@mydavinci.nl>
2024-09-18 15:34:27 +0200

    Update 4 party met naam.py

--- module_1/deel 2/4 party met naam.py
+++ module_1/deel 2/4 party met naam.py
@@ -1,12 +1,12 @@
 naam = input('wat is de gast heer zij/haar naam? ')
 gastheer = False
-gasten = False
+gasten = True
 drank = True
 chips = True
 start_condition_1 = gastheer and gasten and drank
 start_condition_2 =  gastheer or (gasten and drank and chips )
 start_condition_3 =  gastheer and drank 
-if naam.lower() == 'finn' or  start_condition_1 or start_condition_2 and start_condition_3 and not naam.lower()=='wilfred bouman':
+if naam.lower() == 'finn' or  start_condition_1 or start_condition_2 or start_condition_3 and not naam.lower()=='wilfred bouman':
     print('Start the Party')
 else:
     print("no party")
