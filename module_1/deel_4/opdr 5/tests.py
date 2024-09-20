3a24b365b26a871c60fbfebd69bab63d19839232
FINNAPPELO <99080583@mydavinci.nl>
2024-09-20 11:05:48 +0200

    ales voor 11:05

--- module_1/deel_4/opdr 5/tests.py
+++ module_1/deel_4/opdr 5/tests.py
@@ -1,17 +1,19 @@
 #import hier je functie "from [bestandsnaamZonderExtentie] import [functieNaam]"
 from test_lib import test, report
+from De_grootste_function import getal
 
-expected = '' #resultaat
-result = None #roep hier je functie aan met 2 dezelfde getallen
+expected = 'bijde getallen zijn eve groot' #resultaat
+result = getal(2, 2) #roep hier je functie aan met 2 dezelfde getallen
 test('TEST nr1=nr2', expected, result)
 
-expected = '' #resultaat
-result = None #roep hier je functie aan waar nr1 groter is dan nr2
+expected = "Maximum: 19 en minimum: 5" #resultaat
+result = getal(19,5) #roep hier je functie aan waar nr1 groter is dan nr2
 test('TEST nr1>nr2', expected, result)
 
-expected = '' #resultaat
-result = None #roep hier je functie aan  waar nr1 kleiner is dan nr2
+expected = "Maximum: 34 en minimum: 4"
+result = getal(4,34) #roep hier je functie aan  waar nr1 kleiner is dan nr2
 test('TEST nr1<nr2', expected, result)
 
 if __name__ == "__main__":
     report()
+getal(1,5)
