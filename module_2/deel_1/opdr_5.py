27b7256d452f5881d043867ed6da24f432c51993
FINNAPPELO <99080583@mydavinci.nl>
2024-10-07 12:01:32 +0200

    Update opdr_5.py

--- module_2/deel_1/opdr_5.py
+++ module_2/deel_1/opdr_5.py
@@ -1,3 +1,12 @@
-a_lijsjes=int(input('hoeveel lijstjes wilt u zien ? '))
-for i in range(a_lijsjes):
-    e
+def generate_lists():
+    A_lijsten = int(input("Hoeveel lijstjes wilt u zien? "))
+    alle_lijsten = []
+    for i in range(A_lijsten):
+        lengte = int(input(f"Hoelang moet lijst {i + 1} zijn? "))
+        stap = i + 1
+        start = i + 1
+        current_list = [start + stap * j for j in range(lengte)]
+        alle_lijsten.append(current_list)
+
+    print(alle_lijsten)
+generate_lists()
